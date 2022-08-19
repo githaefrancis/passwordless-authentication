a56e2c17e92f081802d8609a0046b0c52125ef16
Francis Githae <mureithigithae@gmail.com>
2022-08-19 17:14:05 +0300

    fix gitignore

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+from users import User
 # Create your tests here.
+class TestUser(TestCase):
+
+
+    def setUp(self):
+        self.new_user=User("francis.githae@quatrixglobal.com")
+
+    def test_create_user(self):
+        self.new_user.save()
+        self.assertTrue(len(User.objects.all())>0)
+
+

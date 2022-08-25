caf3f1ac827ae9fe9f5c1b4a02f5e9275fb40168
Francis Githae <mureithigithae@gmail.com>
2022-08-26 00:13:55 +0300

    update settings

--- backend/authentication/settings.py
+++ backend/authentication/settings.py
@@ -82,8 +82,8 @@ WSGI_APPLICATION = 'authentication.wsgi.application'
 # }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3001",
-    "http://127.0.0.1:3001",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
     "http://127.0.0.1:7500",
   
 ]

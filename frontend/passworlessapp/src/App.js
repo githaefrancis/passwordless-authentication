cfc4b9a4cd8690e2384e64fbdd468474cfe301d3
Francis Githae <mureithigithae@gmail.com>
2022-08-25 22:12:17 +0300

    add routes

--- frontend/passworlessapp/src/App.js
+++ frontend/passworlessapp/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 import Login from './components/Auth/Login';
+import {Routes,Route,Link} from 'react-router-dom'
+import Home from './components/Home/Home';
+import Verify from './components/Auth/Verify';
+
 function App() {
   return (
     <div className="App">
-      <Login />
+      {/* <Login /> */}
+      <Routes>
+        <Route path="/verify" element={<Verify />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Login />} />
+
+      </Routes>
     </div>
   );
 }
---
+++ frontend/passworlessapp/src/components/Auth/Verify.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Verify=()=>{
+
+return(
+<div>Verify</div>
+)
+}
+
+export default Verify
